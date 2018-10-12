ad96f44fc4cbd9ac014dbef10970c548d80c4e67
Duy Nguyen <ngdangduy2009@gmail.com>
2018-10-13 00:28:35 +0700

    merge conflict

--- src/boot/app-navigator.tsx
+++ src/boot/app-navigator.tsx
@@ -3,29 +3,17 @@ import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
 import { Root } from 'native-base';
 import serviceProvider from '../services/service.provider';
 import ScreenNames from '../screens/screen-names';
-<<<<<<< HEAD
-import { Login } from '../screens';
-=======
-import { Test1, Test2, Register } from '../screens';
->>>>>>> 2269228377f465aede0c142e7c978d91c6260a63
+import { Login, Register } from '../screens';
 
 class AppNavigator extends React.Component<any, any> {
 
 	Stack: any = createStackNavigator(
 		{
-<<<<<<< HEAD
 			[ScreenNames.Login]: { screen: Login },
-		},
-		{
-			initialRouteName: ScreenNames.Login,
-=======
-			[ScreenNames.Test1]: { screen: Test1 },
-			[ScreenNames.Test2]: { screen: Test2 },
 			[ScreenNames.Register]: { screen: Register },
 		},
 		{
-			initialRouteName: ScreenNames.Register,
->>>>>>> 2269228377f465aede0c142e7c978d91c6260a63
+			initialRouteName: ScreenNames.Login,
 			headerMode: 'none'
 		}
 	);
